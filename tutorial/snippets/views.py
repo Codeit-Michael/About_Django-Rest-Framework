ffc697a44d29f927eb79ef6101707186fea03157
Michael Maranan <michaelmaranan030@gmail.com>
2021-08-19 18:38:47 +0800

    authentication & permission

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -5,6 +5,10 @@ from rest_framework import generics
 from django.contrib.auth.models import User
 from snippets.serializers import UserSerializer
 
+from rest_framework import permissions
+
+from snippets.permissions import IsOwnerOrReadOnly
+
 
 ## GENERIC CLASS BASED API VIEWS ##
 class SnippetList(generics.ListCreateAPIView):
@@ -14,10 +18,14 @@ class SnippetList(generics.ListCreateAPIView):
 	def perform_create(self, serializer):
 		serializer.save(owner=self.request.user)
 
+	permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 	queryset = Snippet.objects.all()
 	serializer_class = SnippetSerializer
+
+	permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
 
 
 ## USER VIEWS ##
